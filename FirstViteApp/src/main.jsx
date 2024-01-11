353ea469d0b9b3bbe9f67b9b68ebfbd83a50cdb9
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-11 10:59:52 +0530

    i write own Costom Render function in main.jsx

--- FirstViteApp/src/main.jsx
+++ FirstViteApp/src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+
+function MyApp(){
+  return(
+    <div>
+      <h1>
+        costom app | Vaibhav Nagargoje
+      </h1>
+    </div>
+  )
+}
+const reactElement=React.createElement(
+  'a',
+  {href:"http://farmo.in",target:'_blank'},
+  'click here to visit Farmo Official Site '
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
+
+    reactElement()
+  
 )
