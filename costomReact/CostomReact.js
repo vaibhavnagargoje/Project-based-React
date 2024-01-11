a89724544621e2909d80e9dd730411959cf0851b
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-11 10:34:26 +0530

    change === to == costom react.js file

--- costomReact/CostomReact.js
+++ costomReact/CostomReact.js
@@ -10,7 +10,7 @@ function costomRender(reactElement, container) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   for (const  prop in reactElement.props) {
-    if(prop==='children') continue;
+    if(prop=='children') continue;
     domElement.setAttribute(prop, reactElement.props[prop])
     }
     container.appendChild(domElement)
