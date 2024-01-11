4bb10503a0f6c557d5abfd38d460ed3c77c0ea3f
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-11 11:14:00 +0530

    update main.jsx

--- FirstViteApp/src/App.jsx
+++ FirstViteApp/src/App.jsx
@@ -1,10 +1,10 @@
 import MyName from "./MyName"
 function App() {
   
-
+  const MyName = "vaibhavpro";
   return (
     <>
-    <h1> one more time hello word with vite </h1>
+    <h1> one more time hello word with vite and my name is <h2> {MyName}</h2> </h1>
     <MyName></MyName>
     </>
   )
--- FirstViteApp/src/main.jsx
+++ FirstViteApp/src/main.jsx
@@ -13,14 +13,16 @@ function MyApp(){
     </div>
   )
 }
+const UserName = "Mauli"
 const reactElement=React.createElement(
   'a',
   {href:"http://farmo.in",target:'_blank'},
-  'click here to visit Farmo Official Site '
+  'click here to visit Farmo Official Site ',
+  UserName
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
 
-    reactElement()
   
 )
