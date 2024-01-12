adab940a5d73f7804f9398a01bf89e027bc94a3a
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-12 15:23:27 +0530

    counter Hooks done with small project, Add or remove Value

--- CounterHooks/Counter/src/App.jsx
+++ CounterHooks/Counter/src/App.jsx
@@ -3,12 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+function App() {
+
+
+   let [counter,setCounter]=useState(0)
+
+  
+  const addValue= ()=>{
+    console.log("value Added",Math.floor(Math.random()*10));
+    counter+=1
+    setCounter(counter)
+  }
+  const removeValue=()=>{
+    console.log("value Removed");
+    if(counter>1){
+      counter-=1
+
+      setCounter(counter)
+    }
+  }
   return (
     <>
       <h1>Hellow, React Hooks </h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Vallue {counter}</button>
+      <br />
+      <button onClick={removeValue}> Remove Value {counter}</button>
+      <footer>Footer : {counter}</footer>
     </>
   )
 }
