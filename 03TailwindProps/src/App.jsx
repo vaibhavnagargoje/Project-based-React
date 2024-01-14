eb6177efce2f8d455d437f627518fed89363ac7f
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-14 20:19:39 +0530

    add online image in tilwind css in app.jsx

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -8,26 +8,29 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className='bg-green-400 text-black rounded-xl'> Tailwaind test </h1>
+
+      
+<figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://media.istockphoto.com/id/1404885250/photo/mahabaleshwar-and-panchagani-nature-and-hill-areas.webp?s=612x612&w=is&k=20&c=RtkcMImZovRNGcmU1DbsGjQKgzYdMQAxstXBVDfVAes=" alt="" width="384" height="512"/>
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
+    </blockquote>
+    <figcaption>
+      <div>
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </>
   )
 }
