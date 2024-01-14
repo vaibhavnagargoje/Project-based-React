029b2d12f6f8bc91902cfc9fe38f6146c74e0c89
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-14 20:48:19 +0530

    props testing using array or object

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -6,11 +6,19 @@ import Card from './components/card'
 function App() {
   
 
+  let myObj ={
+    username:"Vaibhav",
+    age:23,
+    city:'pune',
+    School:'Fergusson'
+  }
+
+  let newArr=[1,2,4,5,6]
   return (
     <>
       <h1 className='bg-green-400 text-black rounded-xl mb-5'> Tailwaind test </h1>
 
-     <Card></Card>
+     <Card Name ="React Series" obj = {myObj} newArr={newArr}></Card>
 
     </>
   )
--- 03TailwindProps/src/components/card.jsx
+++ 03TailwindProps/src/components/card.jsx
@@ -1,8 +1,9 @@
-
-function Card(){
+import React from "react";
+function Card(props){
+  console.log(props);
   return(
     <>
-     <div class="relative h-[400px] w-[300px] rounded-md">
+     <div className="relative h-[400px] w-[300px] rounded-md">
   <img
     src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
     alt="AirMax Pro"
